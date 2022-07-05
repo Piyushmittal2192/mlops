e0d5da350170bea6f5f637fcb83a98d359d8a85a
Piyush Mittal <piyush.mittal@ericsson.com>
2022-07-05 10:32:35 -0500

    adding files

--- ml_template/src/test_file_transfer.py
+++ ml_template/src/test_file_transfer.py
@@ -4,12 +4,12 @@ def sftpExample():
     cnopts.hostkeys = None
 
 
-    hostname = '138.85.180.211'
-    username = 'emitpiy'
+    hostname = '<>'
+    username = '<>'
     password = ''
 
     with pysftp.Connection(hostname, username=username, password=password, cnopts=cnopts) as sftp:
-        sftp.put('C://Users//emitpiy//OneDrive - Ericsson AB//Projects//mlops//ml_template//ml_template//data_given//winequality.csv','/data/ml_pipeline/mlruns/mlflow_artifacts/winequality.csv')  # upload file to public/ on remote
+        sftp.put('./winequality.csv','/data/ml_pipeline/mlruns/mlflow_artifacts/winequality.csv')  # upload file to public/ on remote
         sftp.close()
 
 sftpExample()
